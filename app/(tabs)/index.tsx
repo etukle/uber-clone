4f36ae70b3bc4ef6aed525fc879cfc9ae26cc115
Evren Tükle <tukleevren@gmail.com>
2024-09-06 12:49:14 +0100

    Fixes index file.

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,10 +6,6 @@ export default function HomeScreen() {
   return (
       <SafeAreaView className="flex-1 items-center justify-center bg-white">
           <Text className={'text-red-500'}>Uber Clone!</Text>
-          <StatusBar style="auto" />
       </SafeAreaView>
   );
 }
-
-
-FJG*zvu9tzp*cxr5hjd
